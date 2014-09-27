cb38cc230b356d6e7a567f4bd8b55a72550a9ed6
Ricardo Castañeda <ricardojudo@gmail.com>
2014-09-27 13:57:56 -0500

    Generando metodos para autenticacion de usuarios mediante API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+  before_save :ensure_authentication_token
 
   validates :first_name, presence: true
   validates :email, uniqueness: true
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
 end
