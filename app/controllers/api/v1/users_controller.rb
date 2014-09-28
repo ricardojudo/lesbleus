f2756fb860786985717839b011accf6d2277aee1
Ricardo Castañeda <ricardojudo@gmail.com>
2014-09-28 06:26:57 -0500

    Exponiendo informacion del usuario

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  before_filter :authenticate_user!
+  
   def index
+    current_user=User.find(params[:token].to_i)
     render json: current_user, status: :ok
   end
 
